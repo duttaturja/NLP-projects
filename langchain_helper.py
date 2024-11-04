fda9685ce3c020df5cbd04d3033093d9a97ffb79
Turja Dutta <133532872+duttaturja@users.noreply.github.com>
2024-11-04 23:27:07 +0600

    final commit

--- langchain_helper.py
+++ langchain_helper.py
@@ -1,20 +1,24 @@
+#getting the ollama model and langchain dependencies
 from langchain_ollama.llms import OllamaLLM
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 
+#function to generate pet names
 def generate_pet_name(animal_type, pet_color, names_amount):
-    model = OllamaLLM(model="llama3.2:1b")
+    model = OllamaLLM(model="llama3.2:1b") #Ollama model to generate pet names
     prompt_template_name = PromptTemplate(
         input_variables=['animal_type', 'pet_color', 'names_amount'],
         template="I have a {animal_type} pet and I want a cool name for it, it is {pet_color} in color. Suggest me {names_amount} cool names for my pet."
-    )
+    ) #prompt to generate pet names with input variables
     name_chain = LLMChain(
         llm=model,
         prompt=prompt_template_name,
         output_key="pet_name",
-    )
+    ) #chain to generate pet names through prompt and Ollama model
+
+    #generating pet name from the chain
     response = name_chain({'animal_type': animal_type, 'pet_color': pet_color, 'names_amount': names_amount})
-    return response
+    return response #returning the pet names
 
 if __name__ == "__main__":
     print(generate_pet_name("Dog", "Black", 5))
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import langchain_helper as lch
 import streamlit as st
 
+#website tilte
 st.title("Pet Name Generator")
 
+#sidebar inputfields 
+
+#user inputs the animal type
 user_animal_type = st.sidebar.selectbox("What is your pet?", ["Dog", "Cat", "Cow", "Hamster"])
 
+#user inputs the pet color based on the animal type
 if user_animal_type == "Cat":
     user_pet_color = st.sidebar.text_area(label="What is your pet's color?", max_chars=100)
 
@@ -17,8 +22,20 @@ if user_animal_type == "Cow":
 if user_animal_type == "Hamster":
     user_pet_color = st.sidebar.text_area(label="What is your pet's color?", max_chars=100)
 
+#user inputs the amount of names to be generated
 pet_name_amount = st.sidebar.text_input(label="How many pet names do you want?")
 
-if pet_name_amount and user_pet_color:
-    response = lch.generate_pet_name(user_animal_type, user_pet_color, pet_name_amount)
-    st.text(response['pet_name'])
+#showing output if user inputs the amount of names to be generated
+if pet_name_amount:
+    response = lch.generate_pet_name(user_animal_type, user_pet_color, pet_name_amount) #calling the function
+    st.text(response['pet_name']) #showing the pet names
+
+    #footer
+    st.markdown("---")
+    st.write("Developed by: duttaturja")
+    st.write("[GitHub](https://github.com/duttaturja) [Facebook](https://www.facebook.com/turja.dutta.313/) [Instagram](https://www.instagram.com/duttaturja)")
+
+#developed by: duttaturja 
+# [GitHub](https://github.com/duttaturja) 
+# [Facebook](https://www.facebook.com/turja.dutta.313/) 
+# [Instagram](https://www.instagram.com/duttaturja)   
